7a70d1a4a0f7682e59087db3582fbf0f8566ef7f
MartinaBasile <martinaleandribasile@gmail.com>
2022-11-17 11:38:00 +0100

    delete previus push

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -103,6 +103,7 @@ class CategoryController extends Controller
             $counter++;
         }
         $category->slug = $slug;
+        $category->save();
         return redirect()->route('admin.categories.show', $category->id);
     }
 
